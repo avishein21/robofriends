a978a408e69e5f5ee58452609503e5ed26c754d6
Avi <47988220+avishein21@users.noreply.github.com>
2022-06-22 14:48:16 +0530

    Add Project

---
+++ build/precache-manifest.81c3efb6a7432edfebfeb4c6fbfb1c01.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "eff6230f27c63fea6531",
+    "url": "./static/css/main.1dca52b7.chunk.css"
+  },
+  {
+    "revision": "eff6230f27c63fea6531",
+    "url": "./static/js/main.eff6230f.chunk.js"
+  },
+  {
+    "revision": "99eeb3f606d81be2dc49",
+    "url": "./static/css/1.ec0e338c.chunk.css"
+  },
+  {
+    "revision": "99eeb3f606d81be2dc49",
+    "url": "./static/js/1.99eeb3f6.chunk.js"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "ca38c8510a8a634b13ae4a71bd2dd479",
+    "url": "./index.html"
+  }
+];
